d2d638df80be95d75fb41e98cad6c721676b85c0
dchen038 <dchen038@gmail.com>
2014-11-29 16:16:38 -0500

    First Commit

--- app/build/generated/source/r/debug/com/example/studyapp/R.java
+++ app/build/generated/source/r/debug/com/example/studyapp/R.java
@@ -16,13 +16,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int scrubber_control_normal_holo=0x7f020001;
+        public static final int scrubber_control_pressed_holo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f050005;
+        public static final int action_settings=0x7f050006;
         public static final int buttonGO=0x7f050004;
         public static final int display_time=0x7f050000;
         public static final int minute_label=0x7f050001;
         public static final int question_time=0x7f050002;
+        public static final int seekBar=0x7f050005;
         public static final int slide_time=0x7f050003;
     }
     public static final class layout {
